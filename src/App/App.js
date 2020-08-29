3af5313cfff903eac5173ee8448b049e0bcc9ff1
Mark Young <markyaaronyoung@gmail.com>
2020-08-29 10:09:45 -0500

    added a loadComponents function that loads components on authentication

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
 import Home from '../components/pages/Home/Home';
@@ -13,15 +15,44 @@ import './App.scss';
 fbConnection();
 
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
+    const loadComponents = () => {
+      if (authed) {
+        return <div>
+          <Home />
+          <SingleBirb />
+          <NewBirb />
+          <EditBirb />
+        </div>;
+      }
+      return <h1>Log in NOW</h1>;
+    };
+
     return (
       <div className="App">
         <h2>BIRB WATCHER</h2>
-        <MyNavbar />
-        <Home />
-        <SingleBirb />
-        <EditBirb />
-        <NewBirb />
+        <MyNavbar authed={authed}/>
+        {loadComponents()}
       </div>
     );
   }
