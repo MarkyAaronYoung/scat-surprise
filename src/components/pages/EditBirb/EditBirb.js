7a85fe069a67e94501e459ab93fb204cdd52542f
Mark Young <markyaaronyoung@gmail.com>
2020-08-29 09:57:51 -0500

    added home, editbirb, newbirb, singlebirb, home components, and auth to firebase

---
+++ src/components/pages/EditBirb/EditBirb.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class EditBirb extends React.Component {
+  render() {
+    return (
+      <div className="EditBirb">
+        <h2>Home</h2>
+      </div>
+    );
+  }
+}
+
+export default EditBirb;
